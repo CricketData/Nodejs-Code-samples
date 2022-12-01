42c72c5d2c123e63f0840dff48f6f17565d06e78
CricketData <119282951+CricketData@users.noreply.github.com>
2022-12-01 16:29:27 +0530

    Create Player_Search.js

---
+++ Player_Search.js
@@ -0,0 +1,15 @@
+var axios  =  require('axios');
+const port = 8080;
+const Name = "Olive Smith"
+const playerName = Name.replace(" ", "%20");
+const apikey = "%APIKEY%";
+axios.get(`https://api.cricapi.com/v1/players?apikey=${apikey}&offset=0&search=${playerName}`)
+.then((result)=>{    
+    var data = result.data.data;
+    console.log("Player Search..............................................");
+    data.forEach(data => {
+        console.log("Name  - "+data.name+" Country - " +data.country);
+    });
+}).catch((err)=>{
+    console.log(err);
+})
